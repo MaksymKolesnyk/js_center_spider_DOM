5365f100ff870a9fbbc622959c8a631ab1d53071
MaksymKolesnyk <maksymka@icloud.com>
2024-08-11 15:48:24 -0400

    make build

---
+++ index.6cd14b20.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),i=e.getBoundingClientRect(),l=t.getBoundingClientRect(),n=i.width,c=i.height,o=l.height,d=l.width;t.style.left=(n-d-e.clientLeft)/2+"px",t.style.top=(c-o-e.clientTop)/2+"px";
+//# sourceMappingURL=index.6cd14b20.js.map
